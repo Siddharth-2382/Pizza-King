404820fd2e880718919846ded40bf473d8001bf5
Siddharth Movaliya <siddraj1992@gmail.com>
2023-07-21 15:25:39 +0530

    add css for Hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import Background from "./components/Background";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Hero />
+      <Background />
+    </>
   );
 }
 
